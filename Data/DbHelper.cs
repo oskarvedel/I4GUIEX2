1a8cecb59e88e2a878aea2823f9a5a35bf22d3ae
oskar <oskar.vedel@gmail.com>
2020-04-19 22:13:24 +0200

    jaa

--- Data/DbHelper.cs
+++ Data/DbHelper.cs
@@ -17,9 +17,61 @@ namespace GUIEX2PROJECT.Data
         public static void SeedData(ApplicationDbContext db, UserManager<Employee> userManager)
         {
             
-            SeedUsers(userManager);
+            SeedEmployees(userManager);
         }
-        public static void SeedUsers(UserManager<Employee> userManager)
+        
+        private static void Seed(ApplicationDbContext db)
+        {
+            var r = db.Rooms.FirstOrDefault();
+            if (r == null)
+            {
+                var rooms = new List<Room>();
+                r = new Room()
+                {
+                    RoomNumber = 1,
+                    NumberOfAdults = 2,
+                    NumberOfChildren = 2
+                };
+                rooms.Add(r);
+                r = new Room()
+                {
+                    RoomNumber = 2,
+                    NumberOfAdults = 2,
+                    NumberOfChildren = 4
+                };
+                rooms.Add(r);
+
+                db.Rooms.AddRange(rooms);
+                db.SaveChangesAsync();
+            }
+            var b = db.BreakfastReservations.FirstOrDefault();
+            if (b == null)
+            {
+                var breakfastReservations = new List<BreakfastReservation>();
+                b = new BreakfastReservation()
+                {
+                    DateTime = DateTime.Today,
+                    NumberOfAdultReservations = ,
+                    NumberOfChildReservations = ,
+                    NumberOfAdultsCheckedIn = ,
+                    NumberOfChildrenCheckedIn = 
+                };
+                musics.Add(m);
+                m = new Music()
+                {
+                    Year = DateTime.Now.Year,
+                    Month = DateTime.Now.Month + 1,
+                    Name = "Tahoe Greg",
+                    Description = "Tahoe Greg&rsquo;s back from his tour. New songs. New stories. CDs are now available.",
+                    ThumbNailUrl = "gregthumb.jpg",
+                    ImageUrl = "greg.jpg"
+                };
+                musics.Add(m);
+                db.Musics.AddRange(musics);
+                db.SaveChangesAsync();
+            }
+        }
+        private static void SeedEmployees(UserManager<Employee> userManager)
         {
             string password = "nicepw_1";
             var user = new Employee
@@ -31,11 +83,37 @@ namespace GUIEX2PROJECT.Data
             };
             
             var result =  userManager.CreateAsync(user, password).Result;
+            
+            user = new Employee
+            {
+                UserName = "alina@gmail.com",
+                Email = "alina@gmail.com",
+                EmployeeId = "2",
+                EmployeeType = EmployeeEnum.Receptionist
+            };
+            
+            result =  userManager.CreateAsync(user, password).Result;
 
             if (result.Succeeded)
             {
-                var chefClaim = new Claim("Chef", "Yes");
-                userManager.AddClaimAsync(user, chefClaim);
+                var receptionistClaim = new Claim("Receptionist", "Yes");
+                userManager.AddClaimAsync(user, receptionistClaim);
+            }
+            
+            user = new Employee
+            {
+                UserName = "alina@gmail.com",
+                Email = "alina@gmail.com",
+                EmployeeId = "3",
+                EmployeeType = EmployeeEnum.Waiter
+            };
+            
+            result =  userManager.CreateAsync(user, password).Result;
+
+            if (result.Succeeded)
+            {
+                var waiterClaim = new Claim("Waiter", "Yes");
+                userManager.AddClaimAsync(user, waiterClaim);
             }
         }
     }

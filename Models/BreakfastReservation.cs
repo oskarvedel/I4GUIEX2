824ba7139cabfc1b45349d0eea2a054ba1c89cc4
SBrahe <201807173@post.au.dk>
2020-04-19 16:11:30 +0200

    modeller

---
+++ Models/BreakfastReservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class BreakfastReservation
+    {
+        enum ChildOrAdult
+        {
+
+        }
+        public class BreakfastReservation
+        {
+            public int ReservationId { get; set; }
+            public DateTime Date { get; set; }
+            public int NumberOfChildReservations { get; set; }
+            public int NumberOfAdultReservations { get; set; }
+            public int NumberOfChildrenCheckedIn { get; set; }
+            public int NumberOfAdultsCheckedIn { get; set; }
+
+            public Room room { get; set; }
+            public int roomNumber { get; set; }
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+
+        public int NumberOfChildren { get; set; }
+        public int NumberOfAdults { get; set; }
+
+        public List<BreakfastReservation> BreakfastDates { get; set; }
+    }
+}

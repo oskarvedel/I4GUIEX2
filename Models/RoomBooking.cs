838742d57b0de81c68c5297ae7810f5834a07b8e
Frederik Runge-Dalager <Frederik Runge-Dalag@LAPTOP-FC9QT90T>
2020-04-20 14:12:18 +0200

    Tam tam tam

--- Models/RoomBooking.cs
+++ Models/RoomBooking.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace GUIEX2PROJECT.Models
 {
     public class RoomBooking
     {
+
+        
         public int BookingId { get; set; }
+        [DisplayName("Date |")]
         public DateTime Date { get; set; }
-        
+
+        [DisplayName(" Children |")]
         public int NumOfChildrenInRoom  { get; set; }
+        [DisplayName(" Adults |")]
         public int NumOfAdultsInRoom  { get; set; }
-        
+        [DisplayName(" Child Reservations")]
         public int NumberOfChildBreakfastReservations { get; set; }
+        [DisplayName(" Adult Reservations |")]
         public int NumberOfAdultBreakfastReservations { get; set; }
-        
+        [DisplayName(" Children Checked In |")]
         public int NumberOfChildrenCheckedInToBreakfast { get; set; }
+        [DisplayName(" Adults Checked In |")]
         public int NumberOfAdultsCheckedInToBreakfast { get; set; }
+        [DisplayName(" Room |")]
         public Room Room { get; set; }
         public int RoomId { get; set; }
         

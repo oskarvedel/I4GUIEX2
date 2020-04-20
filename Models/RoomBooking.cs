9e81a09575539c1e1c8e515aa495cb4224d8de88
oskar <oskar.vedel@gmail.com>
2020-04-20 12:49:59 +0200

    jaa

--- Models/RoomBooking.cs
+++ Models/RoomBooking.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 namespace GUIEX2PROJECT.Models
 {
     public class RoomBooking
     {
         public int BookingId { get; set; }
-        [DisplayName("Date ")]
         public DateTime Date { get; set; }
         
-        [DisplayName("| Children |")]
         public int NumOfChildrenInRoom  { get; set; }
-        [DisplayName(" Adults |")]
         public int NumOfAdultsInRoom  { get; set; }
-
-        [DisplayName(" Children Reservations |")]
+        
         public int NumberOfChildBreakfastReservations { get; set; }
-        [DisplayName(" Adult Reservation |")]
         public int NumberOfAdultBreakfastReservations { get; set; }
-
-        [DisplayName(" Children checked in |")]
+        
         public int NumberOfChildrenCheckedInToBreakfast { get; set; }
-        [DisplayName(" Adult checked in |")]
         public int NumberOfAdultsCheckedInToBreakfast { get; set; }
         public Room Room { get; set; }
         public int RoomId { get; set; }
